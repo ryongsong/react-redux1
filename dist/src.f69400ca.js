b327fd3d37969fa08049892eef98916820d4c81f
具龍星 <ryongsong@guryuuseinoMacBook-Air.local>
2023-08-29 16:17:48 +0900

    react

--- dist/src.f69400ca.js
+++ dist/src.f69400ca.js
@@ -33067,7 +33067,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61314" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62514" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
